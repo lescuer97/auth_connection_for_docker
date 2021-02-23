9a5d3d2fcb3b29ec48832393fed47023e72db660
lescuer97 <leoescuer@protonmail.com>
2021-02-23 16:39:19 +0100

    changed index and changed prettier file
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
- require('dotenv').config();
 
-const { register, login} = require('./controller/userController');
-const app = express()
+require('dotenv').config();
 
-app.use(morgan("dev"));
+const { register, login } = require('./controller/userController');
+const globalErrorHandler = require('./controller/errorController');
+
+const AppError = require('./utils/appError');
+
+const app = express();
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
-
 
 const port = 4001;
 
@@ -18,13 +22,12 @@ app.post('/register', register);
 
 app.get('/login', login);
 
-app.use('*',(req, res) => {
-    res.status(200).json({
-      Response:  "this route doesn't really do anything"
-    });
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
-    console.log(`server listening on port ${port}`)
-})
+  console.log(`server listening on port ${port}`);
+});
